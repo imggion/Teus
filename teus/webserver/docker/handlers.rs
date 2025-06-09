e4846f8e0cf6a77dfd408ea3c6c60972f9b3b731
Giovanni D'Andrea <g.dandrea98@outlook.com>
2025-06-10 01:27:32 +0200

    Add Docker API endpoint handlers for version, containers, and volumes
    - Created a new `handlers.rs` file in the Docker module to define API endpoints for retrieving Docker version, containers, and volumes.
    - Implemented asynchronous handlers for each endpoint, returning JSON responses with appropriate error handling.
    - Added a generic response structure for error messages to standardize API responses.

---
+++ teus/webserver/docker/handlers.rs
@@ -0,0 +1,78 @@
+use crate::{
+    config::{schema::TeusConfig, types::Config},
+    monitor::storage::Storage,
+    webserver::auth::{middleware::Claims, schema::User},
+};
+use actix_web::{get, web, HttpResponse, Responder};
+use argon2::{
+    password_hash::{PasswordHash, PasswordVerifier},
+    Argon2,
+};
+use chrono::{Duration, Utc};
+use docker::docker::{Containers, DockerClient, DockerVersion};
+use jsonwebtoken::{encode, EncodingKey, Header};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GenericDockerResponse {
+    message: String,
+}
+
+#[get("/docker/version")]
+async fn get_docker_version() -> impl Responder {
+    let mut docker_client = DockerClient::new(None);
+    match docker_client.get_version() {
+        Ok(version) => HttpResponse::Ok().json(version),
+        Err(err) => HttpResponse::InternalServerError().json(GenericDockerResponse {
+            message: format!("Error getting docker version: {:?}", err),
+        }),
+    }
+}
+
+#[get("/docker/containers")]
+async fn get_docker_containers() -> impl Responder {
+    let mut docker_client = DockerClient::new(None);
+    match docker_client.get_containers() {
+        Ok(containers) => HttpResponse::Ok().json(containers),
+        Err(err) => HttpResponse::InternalServerError().json(GenericDockerResponse {
+            message: format!("Error getting docker containers: {:?}", err),
+        }),
+    }
+}
+
+#[get("/docker/container/{id}")]
+async fn get_docker_container(id: String) -> impl Responder {
+    let mut docker_client = DockerClient::new(None);
+    let container_id_clone = id.clone();
+    match docker_client.get_container_details(container_id_clone) {
+        Ok(container) => HttpResponse::Ok().json(container),
+        Err(err) => HttpResponse::InternalServerError().json(GenericDockerResponse {
+            message: format!("Error getting docker container {}: {:?}", id, err),
+        }),
+    }
+}
+
+#[get("/docker/volumes")]
+async fn get_docker_volumes() -> impl Responder {
+    let mut docker_client = DockerClient::new(None);
+    match docker_client.get_volumes() {
+        Ok(volumes) => HttpResponse::Ok().json(volumes),
+        Err(err) => HttpResponse::InternalServerError().json(GenericDockerResponse {
+            message: format!("Error getting docker volumes: {:?}", err),
+        }),
+    }
+}
+
+// FIXME: The id is not being passed correctly
+// I think the problem is in the enum DockerApi
+#[get("/docker/volumes/{id}")]
+async fn get_docker_volume(id: String) -> impl Responder {
+    let mut docker_client = DockerClient::new(None);
+    let cloned_id = id.clone();
+    match docker_client.get_volume_details(cloned_id) {
+        Ok(volume) => HttpResponse::Ok().json(volume),
+        Err(err) => HttpResponse::InternalServerError().json(GenericDockerResponse {
+            message: format!("Error getting docker volume {} details: {:?}", id, err),
+        }),
+    }
+}
